a63d1c3bd7d4aa8477d0ebebb9b8ec942aab2d67
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-06-21 23:03:20 +0200

    fix(cors): proxy requests to remote sites
    When running in dev mode, proxy requests to remote sits in order to
    mitigate CORS issues.

--- app/api/index.js
+++ app/api/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+// When running in development/hot mode where the application code is served up via webpack dev server
+// access these remote resources through a proxy that has been defined on the dev server to prevent CORS issues.
+const scheme = process.env.HOT ? '/proxy/' : 'https://'
+
 export function requestTicker(id) {
-  const BASE_URL = `https://api.coinmarketcap.com/v1/ticker/${id}/`
+  const BASE_URL = `${scheme}api.coinmarketcap.com/v1/ticker/${id}`
   return axios({
     method: 'get',
     url: BASE_URL
@@ -19,7 +23,7 @@ export function requestTickers(ids) {
 }
 
 export function requestBlockHeight() {
-  const BASE_URL = 'https://testnet-api.smartbit.com.au/v1/blockchain/blocks?limit=1'
+  const BASE_URL = `${scheme}testnet-api.smartbit.com.au/v1/blockchain/blocks?limit=1`
   return axios({
     method: 'get',
     url: BASE_URL
@@ -29,7 +33,7 @@ export function requestBlockHeight() {
 }
 
 export function requestSuggestedNodes() {
-  const BASE_URL = 'https://zap.jackmallers.com/suggested-peers'
+  const BASE_URL = `${scheme}zap.jackmallers.com//suggested-peers`
   return axios({
     method: 'get',
     url: BASE_URL
