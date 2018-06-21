fe134cdc275e8f9c53b49f188fcd7b55ff5f4aeb
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2018-06-21 21:09:27 +0200

    fix(security): restrictive production CSP
    Adjust our Content Security Policy on production so that it is more
    restrictive than when we run in development mode. Changes include:
    
     - Do not allow loading content/scripts/fonts from localhost
     - Do not allow running code that contains `eval` statements

--- webpack.config.renderer.prod.js
+++ webpack.config.renderer.prod.js
@@ -155,16 +155,9 @@ export default merge.smart(baseConfig, {
     new CspHtmlWebpackPlugin({
       'default-src': "'self'",
       'object-src': "'none'",
-      'connect-src': [
-        "'self'",
-        'http://localhost:*',
-        'ws://localhost:*',
-        'https://api.coinmarketcap.com',
-        'https://zap.jackmallers.com',
-        'https://testnet-api.smartbit.com.au'
-      ],
-      'script-src': ["'self'", 'http://localhost:*', "'unsafe-eval'"],
-      'font-src': ["'self'", 'data:', 'http://localhost:*', 'https://fonts.googleapis.com', 'https://s3.amazonaws.com', 'https://fonts.gstatic.com'],
+      'connect-src': ["'self'", 'https://api.coinmarketcap.com', 'https://zap.jackmallers.com', 'https://testnet-api.smartbit.com.au'],
+      'script-src': ["'self'"],
+      'font-src': ["'self'", 'data:', 'https://fonts.googleapis.com', 'https://s3.amazonaws.com', 'https://fonts.gstatic.com'],
       'style-src': ["'self'", 'blob:', 'https://fonts.googleapis.com', 'https://s3.amazonaws.com', 'https://fonts.gstatic.com', "'unsafe-inline'"]
     })
   ]
