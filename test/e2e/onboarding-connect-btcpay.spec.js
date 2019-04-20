85aea20837e3b2031c4dd04a546f1d01542edf9f
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-04-20 09:34:52 +0200

    test(e2e): enhance test cases

--- test/e2e/onboarding-connect-btcpay.spec.js
+++ test/e2e/onboarding-connect-btcpay.spec.js
@@ -7,8 +7,10 @@ import {
   cleanElectronEnvironment,
 } from './utils/helpers'
 import Onboarding from './pages/onboarding'
+import Loading from './pages/loading'
 
 const onboarding = new Onboarding()
+const loading = new Loading()
 
 fixture('Onboarding (connect btcpay)')
   .page(getBaseUrl())
@@ -38,22 +40,28 @@ test('should connect to an external wallet (btcpay)', async t => {
     .click(onboarding.connectionDetailsTabs.string.button)
     .typeText(
       onboarding.connectionStringInput,
+      /* eslint-disable max-len */
       `{
     "configurations": [
       {
         "type": "grpc",
         "cryptoCode": "BTC",
-        "host": "example.com",
+        "host": "testnet1-lnd.zaphq.io:",
         "port": "19000",
-        "macaroon": "macaroon"
+        "macaroon": "0201036c6e64028a01030a1091304f2d271c450ef775f3bbca0f64101201301a0f0a07616464726573731204726561641a0c0a04696e666f1204726561641a100a08696e766f696365731204726561641a0f0a076d6573736167651204726561641a100a086f6666636861696e1204726561641a0f0a076f6e636861696e1204726561641a0d0a05706565727312047265616400000620bbda8514cda095f98fbfc946efc0b83423e9e48e2cf410e9f27df45feec7a3cb"
       }
     ]
-  }`
+  }`,
+      { paste: true }
     )
     .click(onboarding.nextButton)
 
-    // Confirm connection details and submit
+    // Confirm connection details and submit.
     .expect(onboarding.connectionConfirm.exists)
     .ok()
     .click(onboarding.nextButton)
+
+    // Verify that we show the loading bolt and then the wallet page.
+    .expect(loading.loadingBolt.exists)
+    .ok()
 })
