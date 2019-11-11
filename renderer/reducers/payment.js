efb5a9ed6d8a2f191be07731bd5ad73f0ced1156
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-11-11 23:54:02 +0100

    refactor: improved send payment error handling

--- renderer/reducers/payment.js
+++ renderer/reducers/payment.js
@@ -228,7 +228,7 @@ export const payInvoice = ({
       const payload = {
         paymentId,
         payment_request: payReq,
-        amt: !invoice.millisatoshis && amt,
+        amt: invoice.millisatoshis ? null : amt,
         fee_limit: { fixed: feeLimit },
         timeout_seconds: PAYMENT_TIMEOUT,
       }
@@ -346,6 +346,10 @@ export const paymentFailed = (error, { paymentId }) => async (dispatch, getState
     'payment attempt not completed before timeout', // ErrPaymentAttemptTimeout
     'unable to find a path to destination', // ErrNoPathFound
     'target not found', // ErrTargetNotInNetwork
+    'FAILED_NO_ROUTE',
+    'FAILED_ERROR',
+    'FAILED_TIMEOUT',
+    'TERMINATED_EARLY', // Triggered if sendPayment aborts without giveing a proper response.
   ]
 
   // If we found a related entery in paymentsSending, gracefully remove it and handle as error case.
